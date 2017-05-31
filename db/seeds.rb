ca894325742e34ef7e8f5f73b903f29f3c0f9f50
Rodney Earl <rsearl.90@gmail.com>
2017-05-31 14:41:32 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ Role.create({ name: "instructor" })
 Role.create({ name: "assistant" })
 Role.create({ name: "student" })
 
-user = User.new(fname: "Bowen", password: "password", lname: "Hui", lab: "Instructor", username: "bowenhui@ubc.ca", email: "bowenhui@ubc.ca", instructor: true, studentnumber: 00000000)
+user = User.new(fname: "Bowen", password: "password", lname: "Hui", username: "bowenhui@ubc.ca", email: "bowenhui@ubc.ca", instructor: true, studentnumber: 00000000)
 user.add_role(:instructor)
 user.save
