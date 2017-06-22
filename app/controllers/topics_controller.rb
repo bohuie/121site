b60b7a0b557e8b4fdf316713fb8632a6ee8610f0
Rodney Earl <rsearl.90@gmail.com>
2017-06-22 15:06:49 -0700

    Better dynamic list

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
 
 	def create
 		@user = current_user
-		if @user.assistant || @user.instructor
+		if @user.has_role(:instructor) || @user.has_role(:assistant, Course.find(params[:course_id]))
 			@topic = Topic.create(topic_params)
 			if @topic.save
 				flash[:success] = "Topic created"
@@ -34,7 +34,7 @@ class TopicsController < ApplicationController
 
 	def remove
 		@user = current_user
-		if @user.assistant || @user.instructor
+		if @user.has_role(:instructor, Course.find(params[:course_id])) || @user.has_role(:assistant, Course.find(params[:course_id]))
 			@topic = Topic.find_by(topic_id: params[:id])
 			if(!@topic.nil?)
 				@topic.delete
@@ -46,7 +46,7 @@ class TopicsController < ApplicationController
 		@course = Course.find(params[:course_id])
     	@course_topics = @course.course_topics
     	@course_topic = CourseTopic.new
-    	@topics = Topic.all
+    	@topics = Topic.where.not(id: @course.topics)
     	respond_to do |format|
         	format.js
     	end
