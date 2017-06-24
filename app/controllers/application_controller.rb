a23111a74787374f9754a7b75ef533a07befbdd8
Rodney Earl <rsearl.90@gmail.com>
2017-06-24 11:31:06 -0700

    Catch record not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  around_filter :catch_not_found
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -19,11 +21,18 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def catch_not_found
+    yield
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "There was an error with your search.  Please try again later, or contact an administrator."
+      redirect_back_or root_url
+  end
+
   def check_instructor # Checks current user is an instructor
     if !current_user.has_role? :instructor
       flash[:danger] = 'Instructors only.'
       redirect_to root_path
     end
   end
-  
+
 end
